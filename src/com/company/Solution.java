794f94e11e949dd058ca301e386e8c91de3f5a5b
Karnes <karne1cb@cmich.edu>
2019-10-23 19:09:38 -0400

    My solution ;)

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,22 +5,35 @@ import java.util.List;
 public class Solution implements problem {
     @Override
     public int max(int x, int y) {
-        return 0;
+        return x + y;
     }
 
     @Override
     public String reverse(String s) {
-        return null;
+
+        for(int i = 0; s.length>i; i++;){ //Not done
+
+        }
+
+        return  ;
     }
 
     @Override
     public float bestNumber() {
-        return 0;
+        return 42;
     }
 
     @Override
-    public int sumDigits(int n) {
-        return 0;
+    public int sumDigits(int n) { //Hope
+
+        int temp = 0;
+
+        while(n>0){
+            temp += %n;
+            n/10;
+        }
+
+        return temp;
     }
 
     @Override
