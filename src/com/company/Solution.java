b057e54cf93a56211551809d28f4ba5838f938a2
Crowley <crowl1rr@cmich.edu>
2019-10-23 19:04:59 -0400

    Implemented Max and List Reverse methods

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class Solution implements problem {
     @Override
     public int max(int x, int y) {
-        return 0;
+    	if(x > y) {
+    		return x;
+    	}
+    	else {
+    		return y;
+    	}
     }
 
     @Override
@@ -20,11 +25,16 @@ public class Solution implements problem {
 
     @Override
     public int sumDigits(int n) {
+ 
         return 0;
     }
 
     @Override
     public List reverseList(List l) {
-        return null;
+    	List reverse = new ArrayList;
+    	for(int i = l.size()-1; i >= 0; i--;) {
+    		reverse.add(l.get(i));
+    	}
+        return reverse;
     }
 }
