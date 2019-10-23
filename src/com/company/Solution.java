c0c0ee2836ff95b5ffa9ee29d1cd3aa67b8e8c6d
Nathan Rossow <rosso1n@cmich.edu>
2019-10-23 18:55:34 -0400

    Do all but one of the methods

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,22 +5,33 @@ import java.util.List;
 public class Solution implements problem {
     @Override
     public int max(int x, int y) {
-        return 0;
+        return Math.max(x, y);
     }
 
     @Override
     public String reverse(String s) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(s);
+    	
+    	return sb.reverse().toString();
     }
 
     @Override
     public float bestNumber() {
-        return 0;
+        return 42;
     }
 
     @Override
     public int sumDigits(int n) {
-        return 0;
+    	int retval = 0;
+    	
+    	int i = 1;
+    	while(i <= n) {
+    		retval += i;
+    	}
+    	
+        return retval;
     }
 
     @Override
