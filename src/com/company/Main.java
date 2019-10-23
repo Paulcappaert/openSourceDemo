fd0673a055b3be13292584e39590d7583c312628
kamis1r <33297471+kamis1r@users.noreply.github.com>
2019-10-23 19:10:32 -0400

    commited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("Hello world");
+
+        Solution s = new Solution();
+        System.out.println(s.max(5,3));
+
+        System.out.println(s.reverse("rohit"));
+
+
+
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,12 +5,24 @@ import java.util.List;
 public class Solution implements problem {
     @Override
     public int max(int x, int y) {
+
+        if(x > y){
+            return x;
+        }
+
+        else if(x < y){
+            return y;
+        }
         return 0;
     }
 
     @Override
     public String reverse(String s) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        return sb.reverse().toString();
+
     }
 
     @Override
@@ -20,6 +32,9 @@ public class Solution implements problem {
 
     @Override
     public int sumDigits(int n) {
+
+
+
         return 0;
     }
 
