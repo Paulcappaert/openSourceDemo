43043dc6e1dae0b481dad378e0c67b90182f722e
Nam <nam1w@cmich.edu>
2019-10-23 19:10:27 -0400

    work

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -5,12 +5,17 @@ import java.util.List;
 public class Solution implements problem {
     @Override
     public int max(int x, int y) {
-        return 0;
+        if (x>y) return x;
+        else return y
     }
 
     @Override
     public String reverse(String s) {
-        return null;
+        String str = s;
+        for (int i = s.length; i > 0; i--){
+            str += str.charAt(i)
+        }
+        return str
     }
 
     @Override
