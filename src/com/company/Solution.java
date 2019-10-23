72420e0adcfb04bc8fbe73814a1c0ed7845542b8
Paul Cappaert <paul.cappaert@gmail.com>
2019-10-23 07:46:55 -0400

    initial commit

---
+++ src/com/company/Solution.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.List;
+
+public class Solution implements problem {
+    @Override
+    public int max(int x, int y) {
+        return 0;
+    }
+
+    @Override
+    public String reverse(String s) {
+        return null;
+    }
+
+    @Override
+    public float bestNumber() {
+        return 0;
+    }
+
+    @Override
+    public int sumDigits(int n) {
+        return 0;
+    }
+
+    @Override
+    public List reverseList(List l) {
+        return null;
+    }
+}
---
+++ src/com/company/problem.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.List;
+
+public interface problem {
+
+    /**
+     * @param x
+     * @param y
+     * @return the max of x and y
+     */
+    int max(int x, int y);
+
+    /**
+     *
+     * @param s
+     * @return the reverse string of s
+     */
+    String reverse(String s);
+
+    /**
+     *
+     * @return the best number
+     */
+    float bestNumber();
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    int sumDigits(int n);
+
+    /**
+     *
+     * @param l
+     * @return the reverse list of L
+     */
+    List reverseList(List l);
+
+
+}
